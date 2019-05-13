84431dec7a857bf93512111df664f396d266336c
Greg Morgan <gregory.morgan@flatironschool.com>
2019-05-12 21:30:32 -0700

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,7 @@ class Application
 
     def call(env)
         resp = Rack::Response.new
-        now=Time.now.strftime("%k").to_i
-        if now.to_i <12
+        if Time.now.hour <12
             resp.write "Good Morning"
         else
             resp.write "Good Afternoon"
